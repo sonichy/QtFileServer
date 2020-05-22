c49b40851eb999da4445ec487f859d2f40271259
sonichy <sonichy@163.com>
2020-05-22 16:09:10 +0800

    add readme

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
-#include <QtCore/QCoreApplication>
 #include "qserver.h"
+#include <QtCore/QCoreApplication>
 
 int main(int argc, char *argv[])
 {
     qSetMessagePattern("[ %{file}: %{line} ] %{message}");
-    QCoreApplication a(argc, argv);
-    //QString fullPath = QDir::cleanPath(a.applicationDirPath());
-    QString fullPath = "/media/sonichy/JOB";
+    QCoreApplication a(argc, argv);    
+    QString path = a.applicationDirPath();
     int port = 8000;
-    QServer Server(fullPath, port);
+    QServer Server(path, port);
     Server.StartServer();
     return a.exec();
 }
